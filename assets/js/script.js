5c8b11c264f221051971dfe2179c6b639e3260be
tharvey <tharveyster@gmail.com>
2021-06-07 09:18:43 -0400

    Shortened the method used to assemble the array of available characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ var useLowercase;
 var useUppercase;
 var useNumbers;
 var useSpecialChars;
-var lowerCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numberCharacters = [0,1,2,3,4,5,6,7,8,9];
-var specialCharacters = ["!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/","\:","\;","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 var selectedCharTypes = [];
 var assemblePassword = [];
 var password;
@@ -41,57 +41,41 @@ function generatePassword() {
     alert("You must enter a number between 8 and 128!");
     characterCount = parseInt(prompt("Please choose a password length between 8 and 128 characters"));
   }
-  
+
   useLowercase = confirm("Should the password contain lowercase letters?");
   useUppercase = confirm("Should the password contain UPPSERCASE letters?");
   useNumbers = confirm("Should the password contain numbers?");
   useSpecialChars = confirm("Should the password contain special characaters?");
-  
+
   while (useLowercase === false && useUppercase === false && useNumbers === false && useSpecialChars === false) {
     alert("You must select at least one character type!");
     useLowercase = confirm("Should the password contain lowercase letters?");
     useUppercase = confirm("Should the password contain UPPERCASE letters?");
     useNumbers = confirm("Should the password contain numbers?");
     useSpecialChars = confirm("Should the password contain special characaters?");
-}
+  }
 
-if (useLowercase === true && useUppercase === true && useNumbers === true && useSpecialChars === true) {
-    selectedCharTypes = lowerCharacters.concat(upperCharacters, numberCharacters, specialCharacters);
-  } else if (useLowercase === true && useUppercase === true && useNumbers === true) {
-    selectedCharTypes = lowerCharacters.concat(upperCharacters, numberCharacters);
-  } else if (useLowercase === true && useUppercase === true && useSpecialChars === true) {
-    selectedCharTypes = lowerCharacters.concat(upperCharacters, specialCharacters);
-  } else if (useLowercase === true && useNumbers === true && useSpecialChars === true) {
-    selectedCharTypes = lowerCharacters.concat(numberCharacters, specialCharacters);
-  } else if (useUppercase === true && useNumbers === true && useSpecialChars === true) {
-    selectedCharTypes = upperCharacters.concat(numberCharacters, specialCharacters);
-  } else if (useLowercase === true && useUppercase === true) {
-    selectedCharTypes = lowerCharacters.concat(upperCharacters);
-  } else if (useLowercase === true && useNumbers === true) {
-    selectedCharTypes = lowerCharacters.concat(numberCharacters);
-  } else if (useLowercase === true && useSpecialChars === true) {
-    selectedCharTypes = lowerCharacters.concat(specialCharacters);
-  } else if (useUppercase === true && useNumbers === true) {
-    selectedCharTypes = upperCharacters.concat(numberCharacters);
-  } else if (useUppercase === true && useSpecialChars === true) {
-    selectedCharTypes = upperCharacters.concat(specialCharacters);
-  } else if (useNumbers === true && useSpecialChars === true) {
-    selectedCharTypes = numberCharacters.concat(specialCharacters);
-  } else if (useLowercase === true) {
-    selectedCharTypes = lowerCharacters;
-  } else if (useUppercase === true) {
-    selectedCharTypes = upperCharacters;
-  } else if (useNumbers === true) {
-    selectedCharTypes = numberCharacters;
-  } else {
-    selectedCharTypes = specialCharacters;
+  if (useLowercase === true) {
+    selectedCharTypes = selectedCharTypes.concat(lowerCharacters);
+  }
+
+  if (useUppercase === true) {
+    selectedCharTypes = selectedCharTypes.concat(upperCharacters);
+  }
+
+  if (useNumbers === true) {
+    selectedCharTypes = selectedCharTypes.concat(numberCharacters);
+  }
+
+  if (useSpecialChars === true) {
+    selectedCharTypes = selectedCharTypes.concat(specialCharacters);
   }
 
   for (var i = 0; i < characterCount; i++) {
     var randomChars = selectedCharTypes[Math.floor(Math.random() * selectedCharTypes.length)];
     assemblePassword.push(randomChars);
     finalPassword = assemblePassword.join("");
-  }
+    }
 
   return finalPassword;
 
